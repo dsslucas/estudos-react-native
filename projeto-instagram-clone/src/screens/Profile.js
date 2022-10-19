b6661408f3080e3a1016d981d7b22da5f3a976f1
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-10-19 15:06:27 -0300

    Página de enviar foto

---
+++ projeto-instagram-clone/src/screens/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Gravatar} from 'react-native-gravatar'
+
+class Profile extends Component {
+    logout = () => {
+
+    }
+    render(){
+        const options = {
+            email: 'gil.esfihas@gmail.com',
+            secure: true
+        }
+    }
+}
